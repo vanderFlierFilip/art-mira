eaad474e721ac3b99c0a0925329c4b09cb29c1af
vanderFlierFilip <f.vanderflier@gmail.com>
2021-04-27 17:13:58 +0200

    added redis to project API, implemented basket repository and interface and added basket entity in core project

---
+++ Core/Entities/BasketItem.cs
@@ -0,0 +1,11 @@
+namespace Core.Entities
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public string ArtworkName { get; set; }
+        public int Price { get; set; }
+        public string PictureUrl { get; set; }
+
+    }
+}
