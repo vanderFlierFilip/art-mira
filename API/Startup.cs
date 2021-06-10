0eed84a2098526533a74234530d616ef9eeb6cdf
vanderFlierFilip <f.vanderflier@gmail.com>
2021-06-10 13:00:22 +0200

    modify

--- API/Startup.cs
+++ API/Startup.cs
@@ -65,7 +65,7 @@ namespace API
             app.UseMiddleware<ExceptionMiddleware>();
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseStaticFiles();
